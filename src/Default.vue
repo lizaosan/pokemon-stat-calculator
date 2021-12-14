336f3683781a41bb9e4f0965d50aa026526cda43
Jimmy Y Chen <jimmy.y.chen@itnsl.net>
2021-12-14 14:22:23 +0800

    title

--- src/Default.vue
+++ src/Default.vue
@@ -1,14 +1,15 @@
 <template>
-  <StatCalculator msg="Welcome to Your Vue.js App" />
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import StatCalculator from "./components/StatCalculator.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    StatCalculator,
+    HelloWorld,
   },
 };
 </script>
---
+++ src/components/StatCalculator.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Pokemon Stat Calculator</h1>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
